53543d4561e75c01353a720269f3572eec5db0a5
m.u.nee <mattia.u.nee@gmail.com>
2025-01-31 10:22:49 +0100

    changed init.py

--- rag_evaluator/__init__.py
+++ rag_evaluator/__init__.py
@@ -1,5 +1,5 @@
 from .running_eval_answer import RAGLLMEvaluator
 from .hallucination import RAGHallucinationEvaluator
 from .RAGEvaluationPipeline import RAGEvaluationPipeline
-
-__all__ = ['RAGLLMEvaluator', 'RAGHallucinationEvaluator', 'RAGEvaluationPipeline']
+from .process_text import process_and_search, save_results
+__all__ = ['RAGLLMEvaluator', 'RAGHallucinationEvaluator', 'RAGEvaluationPipeline', 'process_and_search', 'save_results']
