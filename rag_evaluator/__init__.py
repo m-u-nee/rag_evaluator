e2bdc41183ec65d162c8c2b07f5a6734a0c6ecd0
m.u.nee <mattia.u.nee@gmail.com>
2025-02-03 13:47:54 +0100

    added generator to init

--- rag_evaluator/__init__.py
+++ rag_evaluator/__init__.py
@@ -2,4 +2,5 @@ from .running_eval_answer import RAGLLMEvaluator
 from .hallucination import RAGHallucinationEvaluator
 from .RAGEvaluationPipeline import RAGEvaluationPipeline
 from .process_text import process_and_search, save_results
+from .generate import RAGGenerator
 __all__ = ['RAGLLMEvaluator', 'RAGHallucinationEvaluator', 'RAGEvaluationPipeline', 'process_and_search', 'save_results']
